2c5fe508d0a5d103d4d459ec829012391171c6c0
Behzad Pirvali <bpirvali@gmail.com>
2013-09-21 12:46:30 -0700

    initial commit

---
+++ git-proj3/src/com/bp/samples/git/Main.java
@@ -0,0 +1,13 @@
+package com.bp.samples.git;
+
+/*
+ * This is a stupid change
+ * */
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
